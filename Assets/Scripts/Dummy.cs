c09ed87d2b4568578b617bb5b117f8fbabe8cd1a
AngelOswaldo <aornelas@cocoschool.mx>
2020-10-20 13:29:21 -0500

    Dummy

---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Dummy : MonoBehaviour
+{
+    private int mHealth;
+    private int mMeowCount;
+    private string mName;
+    private Vector3 position;
+
+    public Dummy()
+    {
+        mHealth = 10;
+        mMeowCount = 3;
+        mName = "NewDummy";
+        position = Vector3.zero;
+    }
+
+    public byte[] Serialize()
+    {
+        var size = sizeof(int) * 2 + sizeof(float) * 3;
+        byte[] mByte = new byte[size];
+
+        var healthBytes = BitConverter.GetBytes(mHealth);
+
+        for (int i = 0; i < sizeof(int); i++)
+        {
+            mByte[i] = healthBytes[i];
+        }
+
+        var meowBytes = BitConverter.GetBytes(mMeowCount);
+
+        for (int i = 0; i < sizeof(int); i++)
+        {
+            mByte[i + sizeof(int)] = meowBytes[i];
+        }
+
+        var xByte = BitConverter.GetBytes(position.x);
+
+        for (int i = 0; i < sizeof(float); i++)
+        {
+            mByte[i + sizeof(int) * 2] = xByte[i];
+        }
+
+        var yByte = BitConverter.GetBytes(position.y);
+
+        for (int i = 0; i < sizeof(float); i++)
+        {
+            mByte[i + sizeof(int) * 2 + sizeof(float)] = yByte[i];
+        }
+
+        var zByte = BitConverter.GetBytes(position.z);
+
+        for (int i = 0; i < sizeof(float); i++)
+        {
+            mByte[i + sizeof(int) * 2 + sizeof(float) * 2] = zByte[i];
+        }
+
+        return mByte;
+
+    }
+}
--- Assets/Scripts/MyJSON.cs
+++ Assets/Scripts/MyJSON.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
+using System;
 
 public class MyJSON : MonoBehaviour
 {
+    public static MyJSON data;
+
     public string playerName;
     public int lives;
     public float health;
@@ -19,5 +22,32 @@ public class MyJSON : MonoBehaviour
         return JsonUtility.ToJson(this);
     }
 
+    public void Save(string data)
+    {
+        try
+        {
+            File.AppendText(Application.persistentDataPath + "/gamedata.json");
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Save data exception: " + e.Message);
+        }
+
+    }
+
+    public void Load(string path)
+    {
+        try
+        {
+            var json = File.ReadAllText(path);
+            data = JsonUtility.FromJson<MyJSON>(json);
+        }
+        catch(Exception e)
+        {
+            data = new MyJSON();
+            Debug.Log("Load data exception: " + e.Message);
+        }
+
+    }
     
 }
